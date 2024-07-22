0aca811bbb188208ef94ef5af205c7aee60aa0bc
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-07-22 10:57:59 +0500

    Front End Theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createTheme, ThemeProvider  } from '@mui/material'
 
+const theme = createTheme({typography:{fontFamily: "Roboto Slab, serif", allVariants: {color: "white"},},});
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ThemeProvider theme={theme}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </ThemeProvider>
 )
