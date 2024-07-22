3798b554370dc396011334473cf03ae823549595
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-07-22 12:01:35 +0500

    Mods

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { createTheme, ThemeProvider  } from '@mui/material'
+import { BrowserRouter } from "react-router-dom";
+
 
 const theme = createTheme({typography:{fontFamily: "Roboto Slab, serif", allVariants: {color: "white"},},});
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  </ThemeProvider>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    </ThemeProvider>
+  </BrowserRouter>
 )
