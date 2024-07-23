4a74314e0ecc57342bb54255b2fb0bb02f76e0d6
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-07-23 16:06:05 +0500

    Font

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,23 @@
 import { AppBar, Toolbar } from "@mui/material";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+    const auth = useAuth();
     return (
         <AppBar sx={{bgcolor: "transparent", position: "static", boxShadow: "none"}}>
             <Toolbar sx={{display: "flex"}}>
-                <Logo />           
+                <Logo />
+                <div>
+                    {auth?.isLoggedIn? (<>
+                        <NavigationLink bg="#00fffc" to="/chat" text="Go To Chat" textColor="black"/>
+                        <NavigationLink bg="#51538f" textColor="while" to="/" text="logout" onClick={auth.logout}/>
+                    </>) : (<>
+                        <NavigationLink bg="#00fffc" to="/login" text="Login" textColor="black"/>
+                        <NavigationLink bg="#51538f" textColor="while" to="/signup" text="Signup" />
+                    </>)}
+                </div>           
             </Toolbar>
         </AppBar>
     );  
