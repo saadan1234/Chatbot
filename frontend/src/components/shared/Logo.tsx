98cdfa44cf68bfaf0f3e3c1131caeacf349bbfb7
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-07-23 13:57:30 +0500

    Logo Update

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -9,7 +9,7 @@ function Logo() {
         </Link>
         <Typography sx={{display: {md:"block", sm:"none", xs: "none"}, mr: "auto", fontWeight: "800", textShadow: "2px 2px 20px #000"}}>
           <span style={{fontSize: "20px"}}>
-              MERN
+              Creative
           </span>-GPT
         </Typography>
     </div>
