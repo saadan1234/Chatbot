22a7e47e0e8257cc6c1e4960ecae8844e2e32318
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-07-26 14:37:06 +0500

    UI Fixes

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -18,15 +18,15 @@ export const Chat = () => {
   const auth = useAuth();
   const [chatMesseges, setChatMessages] = useState<Message[]>([]);
   const handlesubmit = async () => {
-    const content = inputRef.current?.value as string;
-    if(inputRef && inputRef.current){
-      inputRef.current.value = "";
-    }
-    const newMessage: Message = {role:"user", content};
-    setChatMessages((prev)=>[...prev, newMessage]);
-    //
-    const chatData = await sendChatRequest(content);
-    setChatMessages([...chatData.chats]);
+    // const content = inputRef.current?.value as string;
+    // if(inputRef && inputRef.current){
+    //   inputRef.current.value = "";
+    // }
+    // const newMessage: Message = {role:"user", content};
+    // setChatMessages((prev)=>[...prev, newMessage]);
+    // //
+    // const chatData = await sendChatRequest(content);
+    // setChatMessages([...chatData.chats]);
   };
 
   const handleDeleteChats = async () => {
@@ -60,36 +60,36 @@ export const Chat = () => {
   }, [auth])
 
   return (
-   <Box sx={{display: 'flex', flex:1, width:'100%', height:'100%', mt:3, gap:3}}>
+   <Box sx={{display: 'flex', flex:1, width:'100%', height:'100%',  gap:3}}>
     <Box sx={{display:{md:"flex", xs:"none", sm:"none"}, flex: 0.2, flexDirection: "column"}}>
-      <Box sx={{display: "flex", width:"100%", height: "60vh", bgcolor: "black", borderRadius:"white",  flexDirection: "column", mx :3}}>
+      <Box sx={{display: "flex", width:"100%", height: "95vh", bgcolor: "#333333", borderRadius:3,  flexDirection: "column", mx :3, my:3}}>
         <Avatar sx={{mx: 'auto', my:2, bgcolor: "white", color: 'black', fontWeight:700}}>
           {auth?.user?.name[0]}
         </Avatar>
-        <Typography sx={{mx:'auto', fontFamily: "work sans"}}>
+        <Typography sx={{mx:'auto', fontFamily: "work sans", fontSize:"20px"}}>
         Hi {auth?.user?.name} ! I am MINI-GPT
         </Typography>
-        <Typography sx={{mx:'auto', fontFamily: "work sans", y: 4, p: 3}}>
+        <Typography sx={{ fontFamily: "work sans", y: 4, p: 3, fontSize:"20px"}}>
         Feel free to ask anything from me.
         </Typography>
-        <Button onClick={handleDeleteChats} sx={{width: "200px", my: "auto", color: "black", fontWight: "700", borderRadius: 3, mx: "auto", bgcolor: "white", ":hover": { bgcolor: "#808080"}}}>
+        <Button onClick={handleDeleteChats} sx={{width: "200px", color: "black", my:5,fontWight: "700", borderRadius: 3, mx: "auto", bgcolor: "white", ":hover": { bgcolor: "#808080"}}}>
         Clear Conversation
         </Button>
           
       </Box>
     </Box>
-    <Box sx={{display: "flex", flex: {md: 0.8, xs: 1, sm: 1}, flexDirection: "column", px: 3}}>
-      <Typography sx={{textAlign: "center", fontSize: "40px", color: "white", mb: 2, mx: "auto"}}>
+    <Box sx={{display: "flex", flex: {md: 0.8, xs: 1, sm: 1}, flexDirection: "column", borderRadius: 3, p: 3, m:4, bgcolor:"#333333"}}>
+      <Typography sx={{textAlign: "center", fontSize: "30px", color: "white", mb: 2, mx: "auto"}}>
         Model - GPT 3.5 Turbo
       </Typography>
-      <Box sx={{width: "100%", height: "60vh", borderRadius: 3, mx: "auto", display:"flex", flexDirection: "column", overflow: "scroll", overflowX: "hidden",overflowY: "auto", scrollBehavior: "smooth"}}>
+      <Box sx={{width: "95%", height: "60vh", borderRadius: "10px", mx: "auto", display:"flex", flexDirection: "column", overflow: "scroll", overflowX: "hidden",overflowY: "auto", scrollBehavior: "smooth"}}>
         {chatMesseges.map((chat, index)=> (
           //@ts-ignore 
           <ChatItem content={chat.content} role= {chat.role} key={index}/>
 
         ))}
     </Box>
-    <div style={{width: "100%", padding: "20px", borderRadius: 8, backgroundColor: "black", display: "flex", margin: "auto"}}>
+    <div style={{height:"3%" ,width: "95%", padding: "20px", borderRadius: 8, backgroundColor: "black", display: "flex", marginRight: "30px"}}>
     {" "}
     <input ref={inputRef} type="text" style={{width: "100%", backgroundColor:"transparent", padding: '10px', border: "none", outline: "none" , color: "white", fontSize: "20px"}} />
     <IconButton onClick={handlesubmit}  sx={{ml: "auto", color: "white"}}>
